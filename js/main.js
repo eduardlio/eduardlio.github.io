9899dc4305222a4aef61d1aa3a664732e74a8022
Edward Liao <liao.edward@gmail.com>
2017-05-08 17:15:05 +1000

    Added Scroll functionality and totally edited the thing

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var interests = ["bouldering", "web design", "espresso", "my v60 pourover", "overhangs and dynos", "startups", "reading", "learning", "coffee"];
+var interestCounter = 0;
+setInterval(function(){
+	if(interestCounter==interests.length){interestCounter=0;}
+	var next = interests[interestCounter];
+	var love = document.getElementById("changer");
+	love.innerHTML = next;
+	interestCounter++;
+}, 1000);
+
+
+$(document).ready(function(){
+	randomName();
+})
+function randomName(){
+	var names = ["Eduardo", "Eddie", "Edward", "Ed", "Eddy"];
+	var elem = document.getElementById("name-change");
+	var rName = Math.floor(Math.random()*names.length);
+	var next = names[rName];
+	elem.innerHTML= next;
+}
+$(function (){
+	$.scrollify({
+		section: ".item-wrap",
+		sectionName: ".item-wrap"
+	})
+});
