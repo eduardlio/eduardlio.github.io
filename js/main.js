7b0d3cea3885a3e948b856fea519de089b0a5955
Edward Liao <liao.edward@gmail.com>
2017-05-09 12:56:45 +1000

    Added some and deleted some

--- js/main.js
+++ js/main.js
@@ -14,10 +14,18 @@ $(document).ready(function(){
 })
 function randomName(){
 	var names = ["Eduardo", "Eddie", "Edward", "Ed", "Eddy"];
-	var elem = document.getElementById("name-change");
+	var tunes = ["Stay tuned.", "That's all folks.", "Watch this space.", "Keep on keepin' on."];
+	
+	var name = document.getElementById("name-change");
+	var tune = document.getElementById("tuner");
+	
 	var rName = Math.floor(Math.random()*names.length);
-	var next = names[rName];
-	elem.innerHTML= next;
+	var rTune = Math.floor(Math.random()*tunes.length);
+	
+	var nextName = names[rName];
+	var nextTune = tunes[rTune];
+	name.innerHTML= nextName;
+	tune.innerHTML = nextTune;
 }
 $(function (){
 	$.scrollify({
