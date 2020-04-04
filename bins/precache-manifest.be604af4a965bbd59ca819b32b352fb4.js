d85b728ed536ce7ba06c7660311dbf135e8aca05
eduardlio <liao.edward@gmail.com>
2020-04-04 18:45:00 +1000

    (feature): Add bin days thing

---
+++ bins/precache-manifest.be604af4a965bbd59ca819b32b352fb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c4dc1330bf14e72ffe0906f407a519",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef2897ad22bd0fa4d579",
+    "url": "/static/css/main.1c1a98c5.chunk.css"
+  },
+  {
+    "revision": "bc343e82a5b8f92e1dc2",
+    "url": "/static/js/2.8b90d424.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b90d424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2897ad22bd0fa4d579",
+    "url": "/static/js/main.8c5235a3.chunk.js"
+  },
+  {
+    "revision": "dc1f3b9c1b139cb39897",
+    "url": "/static/js/runtime-main.69e10390.js"
+  }
+]);
