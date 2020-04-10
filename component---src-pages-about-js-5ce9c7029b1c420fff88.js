7c9ff40f19c1fe6768ca775b4da7e001d3987ec4
eduardlio <liao.edward@gmail.com>
2020-04-10 13:42:52 +1000

    Updates

---
+++ component---src-pages-about-js-5ce9c7029b1c420fff88.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"3XHS":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Bl7J"),r=a("vrFN");t.default=function(e){return l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"About"}),l.a.createElement("h1",null,"About me"),l.a.createElement("p",null,"I'm Ed (@eduardlio on pretty much all the socials) and I'm a front end dev at blackbook.ai"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-5ce9c7029b1c420fff88.js.map
